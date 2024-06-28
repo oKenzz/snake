21fbc99adf937478445bf4f677168844b7bbb212
oKenzz <kenz.phu@gmail.com>
2024-06-28 15:58:05 +0200

    Draw the grid

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
+const canvas = document.getElementById('board');
+const gridSize = 50;
+
 const setupCanvas = () => {
-  const canvas = document.getElementById('board');
-  canvas.setAttribute("width", "400");
-  canvas.setAttribute("height", "400");
+  canvas.setAttribute("width", "600");
+  canvas.setAttribute("height", "600");
+  const rows = canvas.height / gridSize;
+  const columns = canvas.width / gridSize;
+  const ctx = canvas.getContext("2d");
+  ctx.strokeStyle = '#99FF8E';
+
+  for (let row = 0; row < rows; row++) {
+    ctx.beginPath();
+    ctx.moveTo(0, row * gridSize);
+    ctx.lineTo(canvas.width, row * gridSize);
+    ctx.stroke();
+  }
+
+
+  for (let column = 0; column < columns; column++) {
+    ctx.beginPath();
+    ctx.moveTo(column * gridSize, 0);
+    ctx.lineTo(column * gridSize, canvas.height);
+    ctx.stroke();
+  }
 };
 
 setupCanvas();
