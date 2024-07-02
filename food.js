e682f286d03b6b00ede8c6fde470b4d7b07b8ea7
Ken Phu <kenz.phu@gmail.com>
2024-07-02 12:25:47 +0200

    Remove and renamed method in Food class

--- food.js
+++ food.js
@@ -2,18 +2,12 @@ class Food {
   constructor(x, y) {
     this.x = x;
     this.y = y;
-    this.spawn();
+    this.renderFood();
   }
 
-  spawn() {
+  renderFood() {
     const ctx = canvas.getContext("2d");
     ctx.fillStyle = "red"
     ctx.fillRect(this.x * 50 + 1, this.y * 50 + 1, gridSize - 2, gridSize - 2);
-  }
-
-  deSpawn() {
-    const ctx = canvas.getContext("2d");
-    ctx.fillStyle = "black"
-    ctx.clearRect(this.x * 50 + 1, this.y * 50 + 1, gridSize - 2, gridSize - 2);
   }
 }
